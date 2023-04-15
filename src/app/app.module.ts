2ad8df33f063407e0b8a6fe44be72d0e624b2458
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-04-15 13:38:24 -0300

    Estilização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostComponent } from './components/post/post.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostComponent,
+    TweetComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
